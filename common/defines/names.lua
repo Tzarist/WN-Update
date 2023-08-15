3bcea47c12808f67d823630d942f08af35714261
Subtrik <Subtriks@gmail.com>
2023-08-15 21:20:58 +0200

    Map Sytle

---
+++ common/defines/names.lua
@@ -0,0 +1,3 @@
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {450, 800, 800}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {450, 800, 800} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800
